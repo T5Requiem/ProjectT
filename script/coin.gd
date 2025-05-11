2190d28b8ebe0341887c50539357f7d8e2507193
Žiga Klepej <147607027+T5Requiem@users.noreply.github.com>
2025-05-11 16:31:16 +0200

    Aseprite support + orb
    Aseprite support integration, coins switched out for balls

--- script/coin.gd
+++ script/coin.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 @onready var player = get_node("../Player")
 
+func _ready() -> void:
+	$orb3.play("default")
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player.collectCoin()
