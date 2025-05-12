90205518d44e2874715ab0be74e2b328632d9103
T5Requiem <147607027+T5Requiem@users.noreply.github.com>
2025-05-12 22:04:14 +0200

    Imported player sprite with running animation
    Imported player sprite with running animation

--- script/player.gd
+++ script/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-@onready var background = get_node("../Background")
-@onready var playerSprite = $PlayerSprite
 @onready var hudBalls = get_node("../Camera/HUD/BallsLabel")
+@onready var playerShape = $PlayerCollisionShape
+@onready var playerAnimationSprite = $PlayerAnimationSprite
 
-@onready var playerSize = playerSprite.texture.get_size() * playerSprite.scale
+@onready var playerSize = playerShape.shape.size
 
 @export var maxPlayerHealth: int = 100
 @export var playerHealth: int = maxPlayerHealth
@@ -12,13 +12,14 @@ extends CharacterBody2D
 @export var gravity: float = 5000.0
 @export var jumpForce: float = 100000.0
 @export var isJumping: bool = false
-@export var balls: int = 0
+@export var isMoving: bool = false
+@export var collectedBalls: int = 0
 
 var ready_to_move := false
 
 func _ready() -> void:
 	add_to_group("player")
-	hudBalls.text = str(balls)
+	hudBalls.text = str(collectedBalls)
 
 	await get_tree().process_frame
 	ready_to_move = true
@@ -30,21 +31,33 @@ func _physics_process(delta: float) -> void:
 	if get_tree().paused:
 		return
 
+	isMoving = false
 	var direction = Vector2.ZERO
-	if Input.is_action_pressed("LEFT") and position.x > 0 + playerSize.x / 2:
-		direction.x -= 1
-	if Input.is_action_pressed("RIGHT") and position.x < 38402 - playerSize.x / 2: 
-		direction.x += 1
+	if Input.is_action_pressed("LEFT") and !Input.is_action_pressed("RIGHT"):
+		if position.x > 0 + playerSize.x / 2:
+			direction.x -= 1
+		playerAnimationSprite.flip_h = true
+		isMoving = true
+	elif Input.is_action_pressed("RIGHT") and !Input.is_action_pressed("LEFT"):
+		if position.x < 3834 * 2 - playerSize.x / 2:
+			direction.x += 1
+		playerAnimationSprite.flip_h = false
+		isMoving = true
 	
 	direction = direction.normalized()
 	velocity.x = direction.x * movementSpeed * delta
 	
 	if not is_on_floor():
+		playerAnimationSprite.play("Idle")
 		if Input.is_action_just_pressed("JUMP") and !isJumping:
 			velocity.y = -jumpForce * delta
 			isJumping = true
 		velocity.y += gravity * delta
 	else:
+		if isMoving:
+			playerAnimationSprite.play("Running")
+		else:
+			playerAnimationSprite.play("Idle")
 		isJumping = false
 		if Input.is_action_just_pressed("JUMP"):
 			velocity.y = -jumpForce * delta
@@ -56,8 +69,8 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 
 func collectBalls() -> void:
-	balls += 1
-	hudBalls.text = str(balls)
+	collectedBalls += 1
+	hudBalls.text = str(collectedBalls)
 
 
 func playerTakeDamage(damage: int) -> void:
